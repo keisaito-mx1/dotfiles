52da5d0ed1e21f5e8042c24beec79dc66eee7237
KeiSaito <saitokei@mx1.wiseman.co.jp>
2023-07-12 08:42:19 +0900

    updaet

--- setup.sh
+++ setup.sh
@@ -1,29 +1,17 @@
 #!/bin/bash
-echo "Xcodeをインストールします..."
+echo "Installing Xcode ..."
 xcode-select --install
 
-echo "homebrewをインストールします..."
+echo "Installing HomeBrew ..."
 which /opt/homebrew/bin/brew >/dev/null 2>&1 || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-echo "brew doctorを実行します..."
-which /opt/homebrew/bin/brew >/dev/null 2>&1 && brew doctor
+echo "Install Brewfile ..."
+which /opt/homebrew/bin/brew >/dev/null 2>&1 && brew bundle --file ./Brewfile --verbose
 
-echo "brew update..."
-which /opt/homebrew/bin/brew >/dev/null 2>&1 && brew update --verbose
-
-echo "brew upgrade..."
-which /opt/homebrew/bin/brew >/dev/null 2>&1 && brew upgrade --verbose
-
-echo "brew install Brewfile"
-which /opt/homebrew/bin/brew >/dev/null 2>&1 && brew bundle --file ./.Brewfile --verbose
-
-echo "brew cleanup..."
-which brew >/dev/null 2>&1 && brew cleanup --verbose
-
-echo "installing asdf plguin"
-./_asdf.sh
+echo "Installing asdf plguin ..."
+./asdf.sh
 
 echo "link .zshrc"
-stow ag zsh -t ~/
+stow -v -d ~/dotfiles -t $HOME zsh
 
 exec $SHELL -l
