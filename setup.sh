fc4f46a82cae183a0a709305a2d8ff8faffb9cf6
kei-mx1 <keisaito@mx1.wiseman.co.jp>
2023-07-14 13:52:16 +0900

    update

--- setup.sh
+++ setup.sh
@@ -4,15 +4,14 @@ xcode-select --install
 
 echo "Installing HomeBrew ..."
 which /opt/homebrew/bin/brew >/dev/null 2>&1 || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-eval "$(/opt/homebrew/bin/brew shellenv)"
 
 echo "Install Brewfile ..."
 which /opt/homebrew/bin/brew >/dev/null 2>&1 && brew bundle --file ./Brewfile --verbose
 
 echo "Installing asdf plguin ..."
-./asdf.sh
+asdf.sh
 
 echo "link .zshrc"
-stow -v -d ~/dotfiles -t $HOME zsh
+stow -v -d ~/dotfiles -t $HOME zsh git asdf
 
 exec $SHELL -l
